ddc814236e6a5812ca2c6318a228add37f05fc6a
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-06 15:15:03 +0100

    Fixed inclusion of files under new structure

--- main.jl
+++ main.jl
@@ -1,5 +1,4 @@
 include("./Core/DataStructures/datastructures.jl")
-include("./Core/GPUAlgorithms/gpualgorithms.jl")
 include("./Core/MPI/mpi.jl")
 include("./Core/Misc/misc.jl")
 include("./Core/Outputs/outputs.jl")

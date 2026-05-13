b477c04004430eb8367ba13238d242418337d049
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-13 19:59:16 +0100

    Changed calculate_theta_updates logic to try to fix incorrect outputs

--- Core/GPUAlgorithms/GPPWPN/calculate_theta_updates.jl
+++ Core/GPUAlgorithms/GPPWPN/calculate_theta_updates.jl
@@ -15,20 +15,20 @@ function calculate_θ_updates_gppwpn!(θ_updates, cells_data, cell_list_params, 
     total_num_threads = workgroup_size * num_workgroups
     kernel! = calculate_θ_updates_gppwpn_kernel!(CUDABackend(), workgroup_size)
     kernel!(θ_updates,
-        cells_data.sorted_particles,
-        cells_data.cell_starts,
-        cells_data.cell_ends,
-        cells_data.cell_neighbours,
-        cell_list_params,
-        min_cell_width,
-        numerical_params.Lx,
-        numerical_params.Ly,
-        numerical_params.R²,
-        numerical_params.Rn²,
-        numerical_params.γ,
-        numerical_params.γn,
-        numerical_params.dt;
-        ndrange=total_num_threads)
+            cells_data.sorted_particles,
+            cells_data.cell_starts,
+            cells_data.cell_ends,
+            cells_data.cell_neighbours,
+            cell_list_params,
+            min_cell_width,
+            numerical_params.Lx,
+            numerical_params.Ly,
+            numerical_params.R²,
+            numerical_params.Rn²,
+            numerical_params.γ,
+            numerical_params.γn,
+            numerical_params.dt;
+            ndrange=total_num_threads)
     KernelAbstractions.synchronize(CUDABackend())
 end #function
 
@@ -79,8 +79,8 @@ end #function
         for stride in Int32(1):num_strides
             if j <= nghbr_cell_end
                 p_j = sorted_particles[j]
-                Δx = p_i.r[1] .- p_j.r[1]
-                Δy = p_i.r[2] .- p_j.r[2]
+                Δx = p_i.r[1] - p_j.r[1]
+                Δy = p_i.r[2] - p_j.r[2]
                 Δx > min_cell_width && (Δx -= Lx)
                 Δx < -min_cell_width && (Δx += Lx)
                 Δy > min_cell_width && (Δy -= Ly)
@@ -93,7 +93,6 @@ end #function
                 if Δx^2 + Δy^2 < Rn²
                     θ_ij = p_j.θ - p_i.θ
                     Fn_sum_lane += Fn(θ_ij, Rn²)
-                    n_lane += Int32(1)
                 end #if
             end #if j
             j += Int32(32)
@@ -127,31 +126,45 @@ end #function
 
     @synchronize()
 
-    # --------- Collect onto warp 0, lane 0 ---------
-    if warp_idx == Int32(0) && lane < Int32(9)
-        F_sum = F_sum_warps[lane+Int32(1)]
-        Fn_sum = Fn_sum_warps[lane+Int32(1)]
-        n = n_warps[lane+Int32(1)]
+    # # --------- Collect onto warp 0, lane 0 ---------
+    # if warp_idx == Int32(0) && lane < Int32(9)
+    #     F_sum = F_sum_warps[lane+Int32(1)]
+    #     Fn_sum = Fn_sum_warps[lane+Int32(1)]
+    #     n = n_warps[lane+Int32(1)]
+    #
+    #     F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(8))
+    #     F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(4))
+    #     F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(2))
+    #     F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(1))
+    #
+    #     Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(8))
+    #     Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(4))
+    #     Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(2))
+    #     Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(1))
+    #
+    #     n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(8))
+    #     n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(4))
+    #     n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(2))
+    #     n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(1))
+    #
+    #     # --------- Store update ---------
+    #     if lane == Int32(0)
+    #         θ_updates[i] = γ * F_sum * dt / n + γn * Fn_sum * dt
+    #     end #if lane
+    # end #if warp_idx
 
-        F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(8))
-        F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(4))
-        F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(2))
-        F_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), F_sum, Int32(1))
+    if warp_idx == Int32(0) && lane == Int32(0)
+        F_sum = 0.0f0
+        Fn_sum = 0.0f0
+        n = Int32(0)
 
-        Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(8))
-        Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(4))
-        Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(2))
-        Fn_sum += CUDA.shfl_down_sync(UInt32(0x000001ff), Fn_sum, Int32(1))
+        for lidx in Int32(1):Int32(9)
+            F_sum += F_sum_warps[lidx]
+            Fn_sum += Fn_sum_warps[lidx]
+            n += n_warps[lidx]
+        end
 
-        n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(8))
-        n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(4))
-        n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(2))
-        n += CUDA.shfl_down_sync(UInt32(0x000001ff), n, Int32(1))
-
-        # --------- Store update ---------
-        if lane == Int32(0)
-            θ_updates[i] = γ * F_sum * dt / n + γn * Fn_sum * dt
-        end #if lane
-    end #if warp_idx
+        θ_updates[i] = γ * F_sum * dt / n + γn * Fn_sum * dt
+    end
 
 end #function

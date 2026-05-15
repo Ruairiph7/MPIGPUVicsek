5e69e7d5d6356110907bd9f0564b7cfd421ac1d4
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-15 15:50:52 +0100

    Removed mentions to Misc/

--- main.jl
+++ main.jl
@@ -1,6 +1,5 @@
 include("./Core/DataStructures/datastructures.jl")
 include("./Core/MPI/mpi.jl")
-include("./Core/Misc/misc.jl")
 include("./Core/Outputs/outputs.jl")
 include("./Core/GPUAlgorithms/gpualgorithms.jl")
 

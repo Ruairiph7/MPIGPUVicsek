a3ac28f0c6e3025bd939fbd413035dcaa0a162de
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-19 12:55:43 +0100

    Readded main_benchmarking.jl

--- main_benchmarking.jl
+++ main_benchmarking.jl
@@ -1,6 +1,8 @@
 include("./Core/DataStructures/datastructures.jl")
+include("./Core/GPUAlgorithms/gpualgorithms.jl")
 include("./Core/MPI/mpi.jl")
 include("./Core/Outputs/outputs.jl")
-include("./Core/GPUAlgorithms/gpualgorithms.jl")
 
 include("./Core/run_simulation_benchmarking.jl")
+
+

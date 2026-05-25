b1a46f3fbcdff8bdfbe6736894d870cc99882928
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-25 16:18:33 +0100

    Fixed issues with benchmarking

--- main_benchmarking.jl
+++ main_benchmarking.jl
@@ -1,6 +1,5 @@
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 #NOTE: For benchmarking:
-@warn "DOING BENCHMARKING..."
 import_time_start = time()
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 
@@ -20,7 +19,11 @@ include("./Core/run_simulation_benchmarking.jl")
 import_time = time() - import_time_start
 writedlm("import_time.txt", import_time)
 
-compilation_time = @elapsed run_simulation(1,1,save_OPs=false,save_plots=false,save_coords=false)
+compilation_time = @elapsed begin
+    MPI.Init()
+    run_simulation(1,1,save_OPs=false,save_plots=false,save_coords=false)
+    MPI.Finalize()
+end #begin
 writedlm("compilation_time.txt", compilation_time)
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 

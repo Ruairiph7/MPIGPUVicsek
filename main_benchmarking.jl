55f3f45fd81bbab5bc7aae1262668344f81696b8
Ruairi Phelan <phelanruairi@gmail.com>
2026-05-25 16:23:04 +0100

    Removed initial compilation run from main_benchmarking.jl as it caused issues with MPI - move to benchmarking script

--- main_benchmarking.jl
+++ main_benchmarking.jl
@@ -18,13 +18,6 @@ include("./Core/run_simulation_benchmarking.jl")
 #NOTE: For benchmarking:
 import_time = time() - import_time_start
 writedlm("import_time.txt", import_time)
-
-compilation_time = @elapsed begin
-    MPI.Init()
-    run_simulation(1,1,save_OPs=false,save_plots=false,save_coords=false)
-    MPI.Finalize()
-end #begin
-writedlm("compilation_time.txt", compilation_time)
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 
 
